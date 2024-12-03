209a785929c9d8635382ad41360e565eb35edda9
Navpreet Singh <navpreetsingh951818@gmail.com>
2024-12-03 18:31:40 +0530

    some changes

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export * from "@/components/table/data-table-pagination";
 export * from "@/components/table/data-table-toolbar";
 export * from "@/components/table/data-table-view-options";
 export * from "@/components/table/page_changer";
+export * from "@/lib/utils";
